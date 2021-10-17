9be13f5ed8531c0058bb75adfc3c47974cf1789f
s-marika <s211935v@st.go.tuat.ac.jp>
2021-10-18 01:56:42 +0900

    [add] addfiles

--- PdfManager.py
+++ PdfManager.py
@@ -4,7 +4,7 @@ import PyPDF2
 def pdf_merger(filelist, savepath):
     merger = PyPDF2.PdfFileMerger()
     for f in filelist:
-        merger.append(f)
+        merger.append(f, import_bookmarks=False)
     merger.write(savepath)
     merger.close()
 
@@ -12,13 +12,13 @@ def pdf_merger(filelist, savepath):
 def pdf_spliter(filename, start_page2, savepath):
     merger = PyPDF2.PdfFileMerger()
     merger.append(filename,
-                  pages=PyPDF2.pagerange.PageRange(':{}'.format(start_page2 - 1)))
+                  pages=PyPDF2.pagerange.PageRange(':{}'.format(start_page2 - 1)), import_bookmarks=False)
     merger.write(savepath)
     merger.close()
 
     merger = PyPDF2.PdfFileMerger()
     merger.append(filename,
-                  pages=PyPDF2.pagerange.PageRange('{}:'.format(start_page2 - 1)))
+                  pages=PyPDF2.pagerange.PageRange('{}:'.format(start_page2 - 1)), import_bookmarks=False)
     merger.write(savepath)
     merger.close()
 
@@ -26,6 +26,6 @@ def pdf_spliter(filename, start_page2, savepath):
 def pdf_extractor(filename, start, stop, savepath):
     merger = PyPDF2.PdfFileMerger()
     merger.append(filename,
-                  pages=PyPDF2.pagerange.PageRange('{}:{}:'.format(start - 1, stop)))
+                  pages=PyPDF2.pagerange.PageRange('{}:{}:'.format(start - 1, stop)), import_bookmarks=False)
     merger.write(savepath)
     merger.close()
